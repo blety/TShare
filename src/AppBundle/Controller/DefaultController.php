2e68a1e2fe027b714031962d9603a7ca5b8e9296
Guillaume Fache <gfache@asi.fr>
2016-03-31 14:15:14 +0200

    Création de l'entité TvShow et du formulaire associé. On réussit à inscrire une série en base.

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -2,9 +2,11 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\TvShow;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use AppBundle\Form\TvShowType;
 
 class DefaultController extends Controller
 {
@@ -13,9 +15,24 @@ class DefaultController extends Controller
      */
     public function indexAction(Request $request)
     {
+        $em = $this->getDoctrine()->getManager();
+        $show = new TvShow();
+        $form = $this->createForm(new TvShowType(), $show);
+
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid()) {
+            $em->persist($show);
+            $em->flush();
+        }
         // replace this example code with whatever you need
         return $this->render('default/index.html.twig', array(
             'base_dir' => realpath($this->container->getParameter('kernel.root_dir').'/..'),
+            'form' => $form->createView()
         ));
+    }
+
+    public function addShowAction(Request $request)
+    {
+        return $this->render('');
     }
 }
---
+++ src/AppBundle/Form/TvShowType.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace AppBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class TvShowType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name')
+            ->add('originalName')
+            ->add('rating')
+            ->add('picture')
+            ->add('seen')
+            ->add('submit', 'submit')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolver $resolver
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'AppBundle\Entity\TvShow'
+        ));
+    }
+}
